76b57e03eb916e5849501034805d615ce3560ca2
rishitha <rishithareddygangula20@gmail.com>
2025-12-02 15:32:41 +0530

    feat(frontend): angular scaffold + placeholder components

---
+++ frontend/src/app/pages/admin-management/admin-management.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-management',
+  imports: [],
+  templateUrl: './admin-management.html',
+  styleUrl: './admin-management.css',
+})
+export class AdminManagement {
+
+}
